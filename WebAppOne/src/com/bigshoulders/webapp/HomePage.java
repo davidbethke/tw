2860ebd4c5b5345362945b98a4e5d50f96f17e29
Dave <david.bethke@gmail.com>
2011-09-03 23:57:16 -0500

    good night

--- WebAppOne/src/com/bigshoulders/webapp/HomePage.java
+++ WebAppOne/src/com/bigshoulders/webapp/HomePage.java
@@ -2,12 +2,15 @@ package com.bigshoulders.webapp;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.Model;
+
 
 
 public class HomePage extends WebPage {
 	public HomePage(){
 		add(new Label("message","Hello World Wicket"));
 		add(new Label("message2","Dave says this!"));
+		add(new Label("message3", new Model<String>("Hello Model")));
 	}
 
 }
